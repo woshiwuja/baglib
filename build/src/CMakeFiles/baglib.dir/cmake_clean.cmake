7a44d59307f8d525d9366cf4180e6b9a5fb0e9c4
woshiwuja <Gabusif@outlook.it>
2025-12-12 14:49:13 +0100

    window surface

---
+++ build/src/CMakeFiles/baglib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/baglib.dir/main.cpp.o"
+  "CMakeFiles/baglib.dir/main.cpp.o.d"
+  "baglib"
+  "baglib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/baglib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/baglib.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for baglib.
+# This may be replaced when dependencies are built.
